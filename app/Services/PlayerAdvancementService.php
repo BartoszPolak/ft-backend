259735e0eeaba56fa9bf7b4ecd46c38e42997a70
Bartosz Polak <bartosz.polak@bavics.com>
2023-12-04 16:53:48 +0100

    Code refactoring

--- app/Services/PlayerAdvancementService.php
+++ app/Services/PlayerAdvancementService.php
@@ -9,30 +9,41 @@ class PlayerAdvancementService
     private const WON_PLAY_POINTS = 20;
     private const NEW_CARDS_ON_LEVEL = 5;
 
+    /**
+     * @var int[]
+     */
+    private readonly array $levels;
+
     public function __construct(private readonly Players $players)
     {
+        $this->levels = config('game.player_levels');
     }
 
     public function playerWonGame(int $playerId): void
     {
         $player = $this->players->find($playerId);
-        $levels = config('game.player_levels');
-
         $player->points += self::WON_PLAY_POINTS;
 
-        $newPlayerLevel = 1;
-        foreach($levels as $level => $maxPoints) {
-            if ($player->points >= $maxPoints) {
-                $newPlayerLevel = $level;
-            }
-        }
+        $playerNewLevel = $this->getPlayerNewLevel($player);
 
-        if ($newPlayerLevel > $player->level) {
-            $player->points -= $levels[$newPlayerLevel];
-            $player->level = $newPlayerLevel;
+        if ($playerNewLevel > $player->level) {
+            $player->points -= $this->levels[$playerNewLevel];
+            $player->level = $playerNewLevel;
             $player->new_cards += self::NEW_CARDS_ON_LEVEL;
         }
 
         $this->players->savePlayerProgress($player);
+    }
+
+    private function getPlayerNewLevel(Players $player): int
+    {
+        $playerNewLevel = 1;
+        foreach($this->levels as $level => $maxPoints) {
+            if ($player->points >= $maxPoints) {
+                $playerNewLevel = $level;
+            }
+        }
+
+        return $playerNewLevel;
     }
 }
